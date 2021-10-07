d2518d760c5d3106d74d2cfafe6e42807a7dc011
Bangia <nitinbangia8@gmail.com>
2021-10-07 20:07:29 +0530

    added test case 6

---
+++ src/test/java/com/employee/framework/testcase6.java
@@ -0,0 +1,11 @@
+package com.employee.framework;
+
+public class testcase6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Test");
+	
+	}
+
+}
